865eb44585a4467f77e7f8a801657a9be86f14cb
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-07-15 16:22:42 -0700

    reproducible research

---
+++ reproduce/get_data.py
@@ -0,0 +1,13 @@
+'''
+Generate random uniform numbers and save to file
+'''
+
+import random
+import csv
+
+
+data = ((i, random.random()) for i in range(10))
+
+with open('data.csv', 'w') as f:
+    pen = csv.writer(f)
+    pen.writerows(data)
