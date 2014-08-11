66dfcf64591e2029119596c89c3217240c1697bb
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-08-11 13:51:38 -0700

    plotting gamma here

--- anova.py
+++ anova.py
@@ -7,8 +7,10 @@ Learning what anova does
 import math
 
 import numpy as np
+from matplotlib import pyplot as plt
 import statsmodels.stats.anova as anova
 from scipy.stats import t
+from scipy.optimize import minimize
 from scipy.special import gamma
 from scipy.misc import factorial
 
@@ -26,7 +28,23 @@ yg = gamma(x)[1:]
 for computed in [ymg, yf, yg]:
     assert np.allclose(computed, ymf)
 
-# TODO - plot the gamma function
+def plot_gamma(left=0.2, right=4):
+    '''
+    plot the gamma function
+    '''
+    x = np.linspace(left, right)
+    y = gamma(x)
+
+    plt.plot(x, y)
+    plt.title('gamma function')
+    plt.show()
+
+plot_gamma()
+
+# It appears that the gamma function has a minimum around 1.5.
+m = minimize(gamma, 1.5)
+print('Minimum for the gamma function on the real line is at {}, '
+      'with a value of {}'.format(m.x[0], m.fun))
 
 # t (Student) distribution
 #t1 = 
