22e9650f3ae539e0a301cdb048cae314ec2338f7
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-09-02 16:22:18 -0700

    double integral

--- integrate.py
+++ integrate.py
@@ -4,7 +4,7 @@ Testing out numerical integration schemes
 
 import numpy as np
 from scipy.stats import norm
-from scipy.integrate import quad
+from scipy.integrate import quad, dblquad
 
 def f(x):
     return (1/3) * x ** (-2/3)
@@ -12,11 +12,11 @@ def f(x):
 def id(x):
     return x
 
+def multi(y, x):
+    return x + y
+
 results = {}
 results['f'] = quad(f, 0, 1)
 results['id'] = quad(id, 0, 1)
 results['norm'] = quad(norm.pdf, -np.infty, np.infty)
-
-# TODO - try multivariate function
-def f(x, y):
-    pass
+results['multi'] = dblquad(multi, 0, 1, lambda x: 0, lambda x: 1)
