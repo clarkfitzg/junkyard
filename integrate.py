7a0baf2ca346c821ddd092bd5d8cad8f3e1a628d
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-09-02 19:49:06 -0700

    messing around with integration

--- integrate.py
+++ integrate.py
@@ -4,7 +4,7 @@ Testing out numerical integration schemes
 
 import numpy as np
 from scipy.stats import norm
-from scipy.integrate import quad, dblquad
+from scipy.integrate import quad, dblquad, nquad
 
 def f(x):
     return (1/3) * x ** (-2/3)
@@ -13,10 +13,15 @@ def id(x):
     return x
 
 def multi(y, x):
-    return x + y
+    return 4 * x * y
+
+def doublex(x, y):
+    return 2 * x + y
 
 results = {}
 results['f'] = quad(f, 0, 1)
 results['id'] = quad(id, 0, 1)
 results['norm'] = quad(norm.pdf, -np.infty, np.infty)
-results['multi'] = dblquad(multi, 0, 1, lambda x: 0, lambda x: 1)
+# not too impressed with this dblquad making y the first arg
+results['multi'] = dblquad(multi, 0, 1, lambda x: 0, lambda x: 1-x)
+results['doublex'] = nquad(doublex, ((0, 1), (0, 2)))
