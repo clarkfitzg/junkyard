8355f88c2398cd260d440891e50c9c99cd5168f5
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-09-26 10:21:03 -0700

    using RCurl get function and RJSONIO

--- rotten.R
+++ rotten.R
@@ -1,9 +1,12 @@
 # Accessing the Rotten Tomatoes API
 
 library(RCurl)
+library(RJSONIO)
 
 tomato_url <- 'http://api.rottentomatoes.com/api/public/v1.0.json'
 
-key <- readChar('~/.rotten_tomatoes_key', 24)
+key <- scan('~/.rotten_tomatoes_key', what='')
 
-firstpage <- postForm(tomato_url, apikey=key)
+raw_content <- getForm(tomato_url, apikey=key)
+
+firstpage <- unlist(fromJSON(raw_content))
