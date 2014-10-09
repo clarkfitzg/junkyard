35aa082bf7a2a29ac0e68bcfb056ef3890835cda
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-10-08 21:23:55 -0700

    starting on math homework

--- STA131A/sets.py
+++ STA131A/sets.py
@@ -10,13 +10,13 @@ from itertools import product
 S = list(product(range(-3, 4), range(0, 5)))
 
 events = {
-        'A': lambda x: x[0] == x[1],
-        'B': lambda x: x[0] == -x[1],
-        'C': lambda x: x[0] ** 2 == x[1] ** 2,
-        'D': lambda x: x[0] **2 + x[1] ** 2 <= 5,
+        'A': [(x, y) for x, y in S if x == y],
+        'B': [(x, y) for x, y in S if x == -y],
+        'C': [(x, y) for x, y in S if x ** 2 == y ** 2],
+        'D': [(x, y) for x, y in S if x ** 2 + y ** 2 <= 5],
 }
 
 print('S =\n', S)
 
-for name, condition in sorted(events.items()):
-    print(name, ' =\n', list(filter(condition, S)))
+for name, event in sorted(events.items()):
+    print(name, ' =\n', event)
