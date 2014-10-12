515384cf3c1372b182367dd766dc388830051347
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-10-12 15:36:56 -0700

    true false questions

---
+++ STA206/hw1/nonlinear.R
@@ -0,0 +1,14 @@
+n = 500
+x = runif(n, max=3 * pi)
+y = 8 * sin(x) + rnorm(n)
+
+mod = lm(y ~ x)
+
+pdf('nonlinear.pdf', width = 5, height = 4)
+
+plot(x, y)
+
+# Fitted line
+abline(mod)
+
+dev.off()
