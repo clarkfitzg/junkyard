449d245d8aece053dc8f7005c60d714aa64d3737
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-10-11 21:08:51 -0700

    linear regression in pure python

--- time_linregress.py
+++ time_linregress.py
@@ -2,8 +2,12 @@
 Some timings for linear regression fitting
 '''
 
+import operator
+import math
+
 import numpy as np
 from scipy import stats
+from collections import deque
 
 
 np.random.seed(14)
@@ -13,11 +17,16 @@ n = 100
 x = np.random.uniform(size=n)
 y = 10 + 4 * x + np.random.randn(n)
 
+# For pure Python
+xlist, ylist = map(list, (x, y))
+xdeque, ydeque = map(deque, (x, y))
+
 # numpy least squares needs the dummy variable
 A = np.vstack([x, np.ones(len(x))]).T
 
+
 # How about a homegrown least squares?
-def lm(x, y):
+def lm_numpy(x, y):
     '''
     Fit least squares to x, y numpy arrays
     '''
@@ -33,12 +42,42 @@ def lm(x, y):
     return b0, b1
 
 
+# Version in pure Python
+
+def dot(x, y):
+    '''
+    The dot product of x and y
+    '''
+    return math.fsum(map(operator.mul, x, y))
+
+
+def mean(x):
+    '''
+    The mean of x
+    '''
+    return math.fsum(x) / len(x)
+
+
+def lm_pure(x, y):
+    '''
+    Fit least squares to x, y Python lists
+    '''
+    xbar = mean(x)
+    ybar = mean(y)
+    
+    xcentered = [x_i - xbar for x_i in x]
+    ycentered = [y_i - ybar for y_i in y]
+
+    b1 = dot(xcentered, y) / dot(xcentered, xcentered)
+    b0 = ybar - b1 * xbar
+
+    return b0, b1
+
+
 # Numpy one is twice as fast
 least_squares = {'numpy': np.linalg.lstsq(A, y),    # 149 us
                  'scipy': stats.linregress(x, y),   # 333 us
-                 'homegrown': lm(x, y),             # 55.9 us
+                 'homegrown': lm_numpy(x, y),       # 56  us
+                 'pure_py': lm_pure(xlist, ylist),  # 117 us
+                 'deque': lm_pure(xdeque, ydeque),  # 119 us
                  }
-
-# TODO write one that uses lists or deques rather than numpy arrays
-
-xlist, ylist = map(list, (x, y))
