8d3259d917c2becf782cd00e81eac46454703190
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-10-28 12:27:42 -0700

    More homework for 206

--- STA206/hw3/hw3.R
+++ STA206/hw3/hw3.R
@@ -6,7 +6,7 @@
 set.seed(328)
 n = 100
 df = data.frame(x1 = runif(n))
-df$y = 400 + 2 * x1 + 0.001 * rnorm(n)
+df$y = 400 + 2 * df$x1 + 0.001 * rnorm(n)
 df$noise = 1e-10 * rnorm(n)
 mod = lm(y ~ x1 + noise, data = df)
 s = summary(mod)
@@ -49,3 +49,89 @@ y = y - 100
 
 mod1o = lm(y ~ x + x2)
 s10 = summary(mod1o)
+
+# 4a
+############################################################
+
+Xfirst4 = matrix(c(rep(1, 4), 0.36, 0.66, 0.66, -0.52,
+                   2.14, 0.74, 1.91, -0.41), nrow=4)
+Xfirst4
+
+# 4b
+############################################################
+
+XtXinv = matrix(c(0.087, -0.014, -0.035, -0.004,
+                  -0.014, 0.115, -0.012, -0.052,
+                  -0.035, -0.012, 0.057, -0.014,
+                  -0.004, -0.052, -0.014, 0.050), nrow=4)
+
+# From ANOVA table
+MSE = 1.040
+degfree = 26
+
+# Predicted value for x1 = 0, x2 = 0 is b0 from summary
+predvalue = 0.9918
+predvalue
+
+# Prediction standard error at this point
+xh = c(1, 0, 0, 0)
+predse = sqrt(MSE * xh %*% XtXinv %*% xh)
+predse
+
+# A 95% prediction interval
+t95 = qt(0.975, degfree)
+interval = c(predvalue - t95, predvalue + t95) * predse
+interval
+
+# 4c
+############################################################
+# Compute sums of squares from the the ANOVA table
+
+SSR = 58.232 + 5.490 + 0.448
+SSR
+SSE = 27.048
+SSTO = SSR + SSE
+SSTO
+
+# 4d
+############################################################
+
+# Read SSR(x1) from ANOVA
+SSR.x1 = 58.232
+
+# SSE(x1)
+SSE.x1 = SSTO - SSR.x1
+SSE.x1
+
+# Read SSR(x2 | x1) from ANOVA
+SSR.x2.given.x1 = 5.490
+SSR.x2.given.x1 
+
+# SSR(x2, x1*x2 | x1)
+5.490 + 0.448
+
+# SSR(x1*x2 | x1, x2)
+0.448
+
+# SSR(x1, x2)
+SSR.x1.x2 = 58.232 + 5.490
+SSR.x1.x2
+
+# SSE(x1, x2)
+SSE.x1.x2 = SSTO - SSR.x1.x2
+SSE.x1.x2
+
+# 4e
+############################################################
+# Coefficients of partial determination
+
+# R2 y, x2 | x1 = SSR(x2 | x1) / SSE(x1)
+R2.given.x1 = SSR.x2.given.x1 / SSE.x1
+R2.given.x1 
+
+# R2 y, x1*x2 | x1, x2 = SSR(x1*x2 | x1, x2) / SSE(x1, x2)
+R2.given.x1.x2 = 0.448 / SSE.x1.x2
+
+# Partial correlation Ry, x1*x2 | x2, x1
+# Sign comes from the coefficient of x1*x2 term in model summary
+-sqrt(R2.given.x1.x2)
