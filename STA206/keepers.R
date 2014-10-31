17909130fa7e69b31b80d08723826067cf531dc8
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-10-31 15:35:01 -0700

    function to make ANOVA table

---
+++ STA206/keepers.R
@@ -0,0 +1,19 @@
+# Functions which may come in handy later
+
+
+anova.table = function(model){
+
+    # Makes an ANOVA table for a model resulting from lm output
+
+    a = anova(model)
+
+    total = data.frame('SS' = sum(a[, 'Sum Sq']), 'DF' = sum(a$Df))
+    error = data.frame('SS' = a['Residuals', 'Sum Sq'], 
+                       'DF' = a['Residuals', 'Df'])
+    regression = data.frame(total - error)
+
+    out = rbind(regression, error, total)
+    out$MS = out$SS / out$DF
+    row.names(out) = c('regression', 'error', 'total')
+    return(out)
+}
