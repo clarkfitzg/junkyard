e98a943a5c4a829f467e1f10311927783e9af3ef
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-12-13 15:40:09 -0800

    1 million variables LP problem in PuLP

--- linearprogramming.py
+++ linearprogramming.py
@@ -1,12 +1,14 @@
 '''
-Linear programming in Python using 3rd party solvers
+Linear programming in Python using COIN OR solvers
 '''
 
 import math
 from pulp import *
 
+n = int(1e4)
+
 # Variables
-v = [LpVariable('x' + str(x)) for x in range(5)]
+v = [LpVariable('x' + str(x)) for x in range(n)]
 
 # Create the 'prob' variable to contain the problem data
 prob = LpProblem('Corner of a Hypercube', LpMaximize)
@@ -21,11 +23,12 @@ for vi in v:
 # The problem is solved using PuLP's choice of Solver
 prob.solve()
 
-prob.writeLP('hypercube.lp')
+#prob.writeLP('hypercube.lp')
 
 # The status of the solution is printed to the screen
 print(LpStatus[prob.status])
 
-# Each of the variables is printed with it's resolved optimum value
-for v in prob.variables():
-    print(v.name, "=", v.varValue)
+## Each of the variables is printed with it's resolved optimum value
+#for v in prob.variables():
+#    print(v.name, "=", v.varValue)
+s = set(vi.varValue for vi in v)
