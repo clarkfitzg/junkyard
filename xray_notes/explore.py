4308cada623b3bf291dccd6fd7e17e0deeeb6574
Clark Fitzgerald <clark.fitzgerald@testimage.corp.climate.com>
2015-06-26 14:23:50 -0700

    some xray notes

--- xray_notes/explore.py
+++ xray_notes/explore.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import xray
 
 # What does squeeze do?
@@ -6,3 +8,13 @@ import xray
 
 a = xray.DataArray(np.random.randn(1, 5))
 a.squeeze()
+
+df = pd.DataFrame({'a': range(5), 'b': range(5)})
+
+# What does the select do?
+df.select(lambda x: x == 2)
+
+(pd.DataFrame({'a': range(5), 'b': range(5)})
+        .mean(skipna=False)
+        .sum()
+)
