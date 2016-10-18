3307945ed97af1e9803f80a33f2d8008e884f63e
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-10-18 16:25:03 -0700

    bokeh app is working

--- bokeh/tutorial/myapp.py
+++ bokeh/tutorial/myapp.py
@@ -1,53 +1,41 @@
 # myapp.py
 
 from time import sleep
-#from random import random
 
-#from bokeh.layouts import column
+from bokeh.layouts import column
 from bokeh.models import Button
-#from bokeh.palettes import RdYlBu3
-from bokeh.plotting import figure, curdoc
+from bokeh.plotting import figure, curdoc, show
 
-# create a plot and style its properties
-p = figure(x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
 
-#p.border_fill_color = 'black'
-#p.background_fill_color = 'black'
-#p.outline_line_color = None
-#p.grid.grid_line_color = None
+plot = figure(x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
 
-# add a text renderer to out plot (no data yet)
-r = p.text(x=[], y=[], text=[], text_color=[], text_font_size="20pt",
-           text_baseline="middle", text_align="center")
+x = list(range(100))
+n = len(x)
 
 i = 0
 
-ds = r.data_source
+# No data yet
+l = plot.line(x=[], y=[])
+ds = l.data_source
 
-# create a callback that will add a number in a random location
+
 def callback():
-    global i
-
-    # BEST PRACTICE --- update .data in one step with a new dict
-    new_data = dict()
-    new_data['x'] = ds.data['x'] + [random()*70 + 15]
-    new_data['y'] = ds.data['y'] + [random()*70 + 15]
-    new_data['text_color'] = ds.data['text_color'] + [RdYlBu3[i%3]]
-    new_data['text'] = ds.data['text'] + [str(i)]
-    ds.data = new_data
-
-    i = i + 1
+    global i, ds
+    y = [i] * n
+    ds.data = {'x': x, 'y': y}
+    i += 1
 
 
 def callback_animate():
-    while True:
+    global i
+    while i < 90:
         callback()
-        sleep(0.5)
+        sleep(0.02)
 
 
-# add a button widget and configure with the call back
+# add a button widget and conploture with the call back
 button = Button(label="Animate")
 button.on_click(callback_animate)
 
 # put the button and plot in a layout and add to the document
-curdoc().add_root(column(button, p))
+curdoc().add_root(column(button, plot))
