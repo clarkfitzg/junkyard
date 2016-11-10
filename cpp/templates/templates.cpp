a1a9bcb0227700676b64cee91e94d7c0acb84afa
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-11-10 12:59:10 -0800

    havent yet got to templates!

--- cpp/templates/templates.cpp
+++ cpp/templates/templates.cpp
@@ -1,7 +1,15 @@
+/* What I've learned:
+ * char *'s include a NULL character at the end, so the length is one more
+ * than the number of characters.
+ * sprintf is called for side effects of modifying char* in place, so it
+ * didn't work when called inline with the << chain.
+ */
+
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
+
 
 int noisy_add(int a, int b)
 {
@@ -9,12 +17,14 @@ int noisy_add(int a, int b)
     return a + b;
 }
 
+
 int main()
 {
-    char one_digit[1];
+    char *one_digit;
     int out = noisy_add(1, 2);
     //std::cout << std::to_string(noisy_add(1, 2));
-    cout << sprintf(one_digit, "%d", out) << "\n";
+    sprintf(one_digit, "%d", out);
+    cout << one_digit << "\n";
     return 0;
 }
 
