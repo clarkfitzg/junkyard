f1a3eee5d0e2d38e3c4590ea79865249316c562a
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-05 11:09:53 -0700

    plot of data sizes

--- dash/dash.R
+++ dash/dash.R
@@ -91,26 +91,72 @@ j3 = fromJSON(raw3)
 # We'll start by grabbing all the metadata for all the datasets.
 
 
-# Initialize
-ds_url = paste0(base_url, "/api/datasets")
-raw = getURLContent(ds_url)
-j = fromJSON(raw)
+get_dash_metadata = function(base_url = "https://dash.ucop.edu"
+                             , first_path = "/api/datasets"
+                             , seconds_between_requests = 0.1)
+{
+    # Start with the first response
+    first_url = paste0(base_url, first_path)
+    j = fromJSON(getURLContent(first_url))
 
-npages = ceiling(j[["total"]] / j[["count"]])
+    npages = ceiling(j[["total"]] / j[["count"]])
 
-if(npages < 2) stop("Expected multiple pages.")
+    # Only 1 page, no need to loop
+    if(npages < 2) return(j[["_embedded"]][[1]])
 
-result = vector(npages, mode = "list")
+    # Preallocate place for each page to go
+    result = vector(npages, mode = "list")
 
-for(i in seq(npages)){
-    # If the API limits the number of calls you can make within some time
-    # period then pause between consecutive requests:
-    # Sys.sleep(2)
-    result[[i]] = j[["_embedded"]][[1]]
-    if(i < npages){
-        nexturl = paste0(base_url, j[["_links"]][["next"]])
-        j = fromJSON(getURLContent(nexturl))
+    for(i in seq(npages)){
+        result[[i]] = j[["_embedded"]][[1]]
+        if(i < npages){
+            nexturl = paste0(base_url, j[["_links"]][["next"]])
+            Sys.sleep(seconds_between_requests)
+            j = fromJSON(getURLContent(nexturl))
+        }
     }
+
+    # Eliminates nesting caused by API structure.
+    do.call(c, result)
 }
 
-out = do.call(c, result)
+
+# Make a data frame with these columns
+nested_to_table = function(x
+    , columns = c("id", "title", "storage_size", "versionNumber")
+){
+    # dash wants to make it as easy as possible to upload your data. To
+    # that end many fields will be missing data. Then it's up to you, the
+    # one processing the metadata, to handle it correctly.
+    # 
+    # Practice for your data munging skills :)
+    #
+    if(!is.list(x)) x = as.list(x)
+    x2 = x[columns]
+    empty = sapply(x2, is.null)
+    x2[empty] = NA
+    out = do.call(data.frame, x2)
+    colnames(out) = columns
+    out
+}
+
+
+md = get_dash_metadata()
+
+tmp = lapply(md, nested_to_table)
+
+meta = do.call(rbind, tmp)
+
+# Now we can look at the storage size
+library(ggplot2)
+
+# Useful for projector
+theme_set(theme_bw(base_size = 20))
+
+ggplot(meta, aes(x = storage_size)) +
+    geom_histogram(bins = 20) +
+    scale_x_log10() +
+    labs(title = "Size of data sets in Dash") +
+    labs(caption = "1e3 = KB, 1e6 = MB, 1e9 = GB, 1e12 = TB")
+
+ggsave("sizes.pdf")
