0e06ce60b29aa76597a14671d7505db0090195f2
Clark Fitzgerald <clarkfitzg@gmail.com>
2020-06-13 14:10:40 -0700

    problem 9

--- julia/euler/9.jl
+++ julia/euler/9.jl
@@ -8,12 +8,21 @@ false
 ```
 """
 pythagorean_triplet = function(a, b, c)
+    a, b, c  = sort([a, b, c])
     a^2 + b^2 == c^2
 end
 
 
 find_triple = function(total = 1000)
     # Only iterating over integers a, b, c that sum to total
-    for a in 1:(total
-
+    for a in 1:(total - 2)
+        for b in 1:(total - a - 1)
+            c = total - a - b
+            if pythagorean_triplet(a, b, c)
+                return a, b, c
+            end
+        end
+    end
 end
+
+prod(find_triple())
