1ca8db21125a4a1d3fc04fc80809c6c6c0ca08fe
Clark Fitzgerald <clarkfitzg@gmail.com>
2020-06-16 17:07:59 -0700

    getting into conversions, and how they work

--- julia/euler/54.jl
+++ julia/euler/54.jl
@@ -1,7 +1,7 @@
 import Base.isless
 
 # A type system for poker hands
-# I'm incrementally building up the classes and methods I need
+# Incrementally build up the classes and methods necessary to compare hands
 
 
 # Card Values
@@ -13,18 +13,30 @@ struct CardValue
 end
 
 function CardValue(value::AbstractString)
-    vals = "2 3 4 5 6 7 8 9 10 J Q K A"
+    vals = "2 3 4 5 6 7 8 9 T J Q K A"
     val_order = split(vals)
     idx = findfirst(x -> x == value, val_order)
+    #@assert !isnothing(idx)
     CardValue(idx)
 end
 
-# Should see two methods now
+# Surprising that this is Any.
+# I expected AbstractChar and AbstractString to share a common supertype
+supertype(AbstractChar)
+
+function CardValue(value::AbstractChar)
+    vs = string(value)
+    CardValue(vs)
+end
+
+
+# Should see some methods now
 methods(CardValue)
 
-# Try out this newly defined constructor
+# Try out the newly defined constructors
 jack = CardValue("J")
-three = CardValue("3")
+three = CardValue('3')
+
 
 function isless(x::CardValue, y::CardValue)
     x.value < y.value
@@ -42,9 +54,13 @@ struct Card
     suit::Char
 end 
 
-function Card(x)
 
-jh = Card(
+jackohearts = Card('J', 'H')
+
+
+function Card(x)
+end
+
 
 
 abstract type Hand end
