d06f6345367fc230e90fb634e4c6701209d5f9f9
Clark Fitzgerald <clarkfitzg@gmail.com>
2020-06-18 17:42:45 -0700

    starting to get a little bored with these poker hands

--- julia/euler/54.jl
+++ julia/euler/54.jl
@@ -3,7 +3,7 @@ if false
     includet("54.jl")
 end
 
-import Base.isless
+#import Base.isless
 
 
 # A type system for poker hands
@@ -14,8 +14,8 @@ import Base.isless
 # ============================================================
 
 # It's probably easier to map these symbols internally to integers to make comparison with < easier
-struct CardValue
-    value::Integer
+struct CardValue{T<:Integer}
+    value::T
 end
 
 
@@ -25,7 +25,7 @@ function CardValue(value::AbstractString)
     vals = "2 3 4 5 6 7 8 9 T J Q K A"
     val_order = split(vals)
     idx = findfirst(z -> z == value, val_order)
-    #@assert !isnothing(idx)
+    @assert !isnothing(idx)
     CardValue(idx)
 end
 
@@ -65,17 +65,30 @@ struct Card
 end 
 
 
-# Is this necessary?
-#function convert(CardValue, x)
-#    CardValue(x)
-#end
+function Card(value, suit)
+    v = CardValue(value)
+    Card(v, suit)
+end
 
 
 jackohearts = Card('J', 'H')
 
 
-function Card(x)
+function Card(x::AbstractString)
+    @assert length(x) == 2
+    Card(x[1], x[2])
 end
+
+queenspades = Card("QS")
+
+# Because they share the same implementation I could do this:
+# function Base.isless(x::Union{CardValue, Card}, y::Union{CardValue, Card})
+
+function Base.isless(x::Card, y::Card)
+    x.value < y.value
+end
+
+@assert jackohearts < queenspades
 
 
 # Hands
@@ -83,6 +96,94 @@ end
 
 abstract type Hand end
 
-struct Pair <: Hand
-    card::Card
+# Sorted with the highest Cards first
+SortedCards = Tuple{Card}
+
+struct HighCard <: Hand
+    cards::SortedCards
 end
+
+
+function HighCard(cards)
+    HighCard(sort(cards))
+end
+
+
+struct Pair <: Hand
+    pair::SortedCards
+    other::SortedCards
+end
+
+
+function Pair(cards)
+    vals = 
+end
+
+
+struct TwoPair <: Hand
+    highpair::SortedCards
+    lowpair::SortedCards
+    other::SortedCards
+end
+
+
+struct Threeuva <: Hand
+    three::SortedCards
+    other::SortedCards
+end
+
+
+struct Straight <: Hand
+    three::SortedCards
+    other::SortedCards
+end
+
+
+struct Flush <: Hand
+    cards::SortedCards
+end
+
+
+struct FullHouse <: Hand
+    three::SortedCards
+    pair::SortedCards
+end
+
+
+struct Fouruva <: Hand
+    four::SortedCards
+    other::SortedCards
+end
+
+
+struct StraightFlush <: Hand
+    cards::SortedCards
+end
+
+
+
+
+"""
+High Card: Highest value card.
+One Pair: Two cards of the same value.
+Two Pairs: Two different pairs.
+Three of a Kind: Three cards of the same value.
+Straight: All cards are consecutive values.
+Flush: All cards of the same suit.
+Full House: Three of a kind and a pair.
+Four of a Kind: Four cards of the same value.
+Straight Flush: All cards are consecutive values of same suit.
+"""
+function Hand(x::AbstractString)
+    cards = split(x)
+    cards = Card.(cards)
+    for H in [StraightFlush, Fouruva, FullHouse, Flush, Straight, Threeuva, TwoPair, OnePair, HighCard]
+        try 
+            return H(cards)
+        catch e
+        end
+    end
+end
+
+
+#pairfives = Hand("5H 5C 6S 7S KD")
